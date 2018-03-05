b390107d3548f1d4dcb867b7274f85853edd4757
cocky <smile.xiaohao@gmail.com>
2018-03-05 20:19:02 +0800

    新增index.jsp页面，新增HelloController，以及配置文件MyMvcConfig

---
+++ src/main/java/com/cocky/springmvc/HelloController.java
@@ -0,0 +1,16 @@
+package com.cocky.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller  // 声明是一个控制器
+public class HelloController {
+
+    // 利用RequestMapping配置Url与方法之间的映射
+    @RequestMapping("/index")
+    public String hello() {
+        // 通过上面ViewResolver的Bean配置，返回值为index，
+        // 说明我的页面配置路径为"/WEB-INF/classes/views/index.jsp
+        return "index";
+    }
+}
---
+++ src/main/java/com/cocky/springmvc/WebInitializer.java
@@ -0,0 +1,28 @@
+package com.cocky.springmvc;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.Registration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.ServletRegistration.Dynamic;
+
+//  WebApplicationInitializer是Spring提供用来配置Servlet 3.0+配置的接口，
+// 从而实现了替代web.xml的位置。实现的接口将会被SpringServletContainerInitializer(用来启动Servlet3.0容器)获取到
+public class WebInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        // 新建WebApplicationContext，注册配置类并将其和当前ServletContext
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(MyMvcConfig.class);
+        ctx.setServletContext(servletContext);
+
+        // 注册SpringMVC，DispatcherServlet
+        Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+        servlet.addMapping("/");
+        servlet.setLoadOnStartup(1);
+    }
+}
