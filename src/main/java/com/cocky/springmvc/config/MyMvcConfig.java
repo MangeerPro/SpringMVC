966bfff9ebeca28be2f2d3f8fc2e0750644134fa
cocky <smile.xiaohao@gmail.com>
2018-03-08 18:56:23 +0800

    Spring MVC其他参数配置

--- src/main/java/com/cocky/springmvc/config/MyMvcConfig.java
+++ src/main/java/com/cocky/springmvc/config/MyMvcConfig.java
@@ -4,10 +4,7 @@ import com.cocky.springmvc.interceptor.DemoInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -39,8 +36,23 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter{
         registry.addInterceptor(demoInterceptor());
     }
 
+    /**
+     * 添加页面跳转
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("/index");
+    }
+
     @Bean  //配置拦截器的Bean
     public DemoInterceptor demoInterceptor() {
         return new DemoInterceptor();
+    }
+
+    //设置不忽略路径参数.后的值 http://localhost:8080/advice.xx 默认情况xx会被忽略
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseSuffixPatternMatch(false);
     }
 }
